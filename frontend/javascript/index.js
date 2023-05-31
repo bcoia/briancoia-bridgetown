c3c442703931f3317fcf7874c064f66fbcda2929
Brian Coia <brian@briancoia.com>
2023-05-31 16:08:47 -0500

    Lazysizes

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -3,6 +3,8 @@ import "syntax-highlighting.css"
 import * as Turbo from "@hotwired/turbo"
 import { Application } from "@hotwired/stimulus"
 
+import "lazysizes"
+
 // Uncomment the line below to add transition animations when Turbo navigates.
 // We recommend adding <meta name="turbo-cache-control" content="no-preview" />
 // to your HTML head if you turn on transitions. Use data-turbo-transition="false"
